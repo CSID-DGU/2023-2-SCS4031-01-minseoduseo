772906e91be57999ab83dc2bdd329dbf976edf9d
seoyeun0106 <sarah.mun16@gmail.com>
2023-11-08 12:32:18 +0900

    [feat] 메뉴 및 라우팅 설정

---
+++ frontend/reactweb/src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./views/Home";
+import "styles/global.scss";
+import "styles/fonts.scss";
+import Router from "router/Router";
+function App() {
+  return (
+    <div className="App">
+      <Router />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/reactweb/src/router/Routes.ts
@@ -0,0 +1,11 @@
+const Routes = {
+  Home: "/",
+  Diary: "/diary",
+  DiaryWrite: "/diary/edit",
+  Dictionary: "/dict",
+  MyInfo: "/my-info",
+  Enroll: "/my-info/enroll",
+  Login: "/login",
+};
+
+export default Routes;
