4bafd771be0043ebea2ad05db7abee0133eb967d
seoyeun0106 <sarah.mun16@gmail.com>
2023-11-08 11:25:53 +0900

    [feat] 홈 메인 페이지 구현

---
+++ frontend/reactweb/src/views/Home.tsx
@@ -0,0 +1,30 @@
+import "styles/fonts.scss";
+import COLOR from "styles/colors";
+import styled from "styled-components";
+import TopSection from "components/Home/TopSection";
+import MenuSection from "components/Home/MenuSection";
+import Header from "components/common/Header";
+
+export default function Home() {
+  return (
+    <StyledMainContainer>
+      <Header icon="menu" title="민서두서" />
+      <StyledMain>
+        <TopSection />
+        <MenuSection />
+      </StyledMain>
+    </StyledMainContainer>
+  );
+}
+const StyledMainContainer = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background: ${COLOR.BG_GRAY_F5};
+`;
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 2rem;
+`;
