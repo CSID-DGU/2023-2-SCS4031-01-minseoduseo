d7e87f088f72c33552aca9d7d37a66747db79b0f
doubleDRG <dyl660115@gmail.com>
2023-11-13 17:39:46 +0900

    [feat]카카오 네이버 callback uri 변경

--- src/main/java/msds/homefarming/auth/kakao/KakaoAuthServlet.java
+++ src/main/java/msds/homefarming/auth/kakao/KakaoAuthServlet.java
@@ -8,21 +8,28 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+
 @Slf4j
 @WebServlet(name = "kakaoRedirectServlet", urlPatterns = "/kakao/login")
 public class KakaoAuthServlet extends HttpServlet
 {
     public final static String KAKAO_AUTH_CODE_URI = "https://kauth.kakao.com/oauth/authorize";
-    
+
     public static String KAKAO_AUTH_CLIENT_ID = "f698ae044a771060c21588e56dcb50f0";
-//    public static String KAKAO_AUTH_REDIRECT_URI = "http://localhost:8080/kakao/callback";
-    public static String KAKAO_AUTH_REDIRECT_URI = "http://homefarm-env.eba-c9f3xbye.ap-northeast-2.elasticbeanstalk.com/kakao/callback";
+    //    public static String KAKAO_AUTH_REDIRECT_URI = "http://localhost:8080/kakao/callback";
+    //==EC2서버 V1==//
+    //    public static String KAKAO_AUTH_REDIRECT_URI = "http://homefarm-env.eba-c9f3xbye.ap-northeast-2.elasticbeanstalk.com/kakao/callback";
+
+    //==EC2서버 V2==//
+    public static String KAKAO_AUTH_REDIRECT_URI = "Homefarming-v2-env.eba-fjp82mhn.ap-northeast-2.elasticbeanstalk.com/kakao/callback";
+
     public static String KAKAO_AUTH_RESPONSE_TYPE = "code";
 
     static String KAKAO_CODE_REQUEST_QUERY_PARAMETER = "?"
             + "client_id=" + KAKAO_AUTH_CLIENT_ID + "&"
             + "redirect_uri=" + KAKAO_AUTH_REDIRECT_URI + "&"
             + "response_type=" + KAKAO_AUTH_RESPONSE_TYPE;
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
--- src/main/java/msds/homefarming/auth/naver/NaverAuthServlet.java
+++ src/main/java/msds/homefarming/auth/naver/NaverAuthServlet.java
@@ -16,8 +16,13 @@ public class NaverAuthServlet extends HttpServlet
     public static String NAVER_AUTH_CODE_URI = "https://nid.naver.com/oauth2.0/authorize";
     public static String NAVER_AUTH_CLIENT_ID = "PHCg_5rwDgHfJ4YlvmAW";
     public static String NAVER_AUTH_RESPONSE_TYPE = "code";
-//    public static String NAVER_AUTH_REDIRECT_URI = "http://localhost:8080/naver/callback";
-    public static String NAVER_AUTH_REDIRECT_URI = "http://homefarm-env.eba-c9f3xbye.ap-northeast-2.elasticbeanstalk.com/naver/callback";
+    //    public static String NAVER_AUTH_REDIRECT_URI = "http://localhost:8080/naver/callback";
+    //==EC2서버 V1==//
+    //    public static String NAVER_AUTH_REDIRECT_URI = "http://homefarm-env.eba-c9f3xbye.ap-northeast-2.elasticbeanstalk.com/naver/callback";
+
+    //==EC2 서버V2==//
+    public static String NAVER_AUTH_REDIRECT_URI = "Homefarming-v2-env.eba-fjp82mhn.ap-northeast-2.elasticbeanstalk.com/naver/callback";
+
     public static String NAVER_AUTH_STATE = "msdshomefarmingapplication";
 
     static String NAVER_CODE_REQUEST_QUERY_PARAMETER = "?"
@@ -30,6 +35,6 @@ public class NaverAuthServlet extends HttpServlet
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
     {
         response.setStatus(HttpServletResponse.SC_FOUND);
-        response.setHeader("Location",NAVER_AUTH_CODE_URI + NAVER_CODE_REQUEST_QUERY_PARAMETER);
+        response.setHeader("Location", NAVER_AUTH_CODE_URI + NAVER_CODE_REQUEST_QUERY_PARAMETER);
     }
 }
