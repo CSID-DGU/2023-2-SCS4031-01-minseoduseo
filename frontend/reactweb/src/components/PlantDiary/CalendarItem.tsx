7dcfa8d15c7d3e63252d2935b9002bf7e155e138
seoyeun0106 <sarah.mun16@gmail.com>
2023-11-17 16:00:15 +0900

    [feat] 일지 하나 당 태그 하나로 변경

--- frontend/reactweb/src/components/PlantDiary/CalendarItem.tsx
+++ frontend/reactweb/src/components/PlantDiary/CalendarItem.tsx
@@ -6,10 +6,7 @@ export default function CalendarItem() {
   return (
     <StyledContainer>
       <StyledTitle>고구마 식물일지 3일차</StyledTitle>
-      <StyledTagContainer>
-        <Tag type="고구마" color="#b75d6e" />
-        <Tag type="토마토" color="#af1a1a" />
-      </StyledTagContainer>
+      <Tag type="고구마" color="#863241" />
     </StyledContainer>
   );
 }
@@ -19,11 +16,6 @@ const StyledContainer = styled.section`
   height: 9rem;
   box-shadow: 0px 6px 5px 0px rgba(13, 63, 103, 0.1);
   padding: 2rem;
-`;
-
-const StyledTagContainer = styled.div`
-  display: flex;
-  gap: 0.6rem;
 `;
 
 const StyledTitle = styled.h3`
